9f22ccc8a0406f299c76448bb5b543e883f6110d
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 15:59:59 +0300

    menü itemlerine tıklama işleminin kontrolü için MainActivity de onOptionsItemSelected metodu yazıldı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
@@ -1,11 +1,13 @@
 package com.ogocer.materialdesignkullanimi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -28,5 +30,28 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar_menu,menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_recyclerview:
+
+                return true;
+            case R.id.action_bottomnavigationview:
+
+                return true;
+            case R.id.action_navigationdrawer:
+
+                return true;
+            case R.id.action_tablayout:
+
+                return true;
+            default:
+                return false;
+        }
+
+
     }
 }
