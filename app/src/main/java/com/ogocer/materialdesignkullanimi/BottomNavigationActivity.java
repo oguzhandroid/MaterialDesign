d874969f16dde0440e3a378176043a8a7b22eb1f
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 17:13:15 +0300

    bottom navigation itemlerine tıklandığında ilgili fragmentin açılması için gerekli kodlamalar yapıldı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
@@ -1,9 +1,11 @@
 package com.ogocer.materialdesignkullanimi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,6 +23,27 @@ public class BottomNavigationActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_tutucu,new FragmentBirinci()).commit();
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_bir){
+                    geciciFragment = new FragmentBirinci();
+                }
+                if (item.getItemId() == R.id.action_iki){
+                    geciciFragment = new FragmentIkinci();
+                }
+                if (item.getItemId() == R.id.action_uc){
+                    geciciFragment = new FragmentUcuncu();
+                }
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_tutucu,geciciFragment).commit();
+                return true;
+            }
+        });
 
     }
+
+
+
 }
