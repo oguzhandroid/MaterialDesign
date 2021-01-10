fabbc896483565c43fef99463761e530a6ab6013
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 16:29:45 +0300

    recyclerView görsel nesnesi RecyclerViewActivity de tanımlandı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/RecyclerViewActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/RecyclerViewActivity.java
@@ -1,14 +1,20 @@
 package com.ogocer.materialdesignkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class RecyclerViewActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+        rv = findViewById(R.id.recyclerView);
     }
 }
