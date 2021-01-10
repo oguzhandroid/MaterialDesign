a708da2f876904b6ac40bc840fe42ac8500869be
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 22:57:47 +0300

    ilgili fragment layoutlar için classlar yazıldı.

---
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/NavFragmentIki.java
@@ -0,0 +1,19 @@
+package com.ogocer.materialdesignkullanimi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NavFragmentIki extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_iki_navlayout,container,false);
+    }
+}
