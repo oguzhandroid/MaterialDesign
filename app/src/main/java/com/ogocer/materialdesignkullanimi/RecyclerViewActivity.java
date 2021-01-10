bdfe4f06d1f42c8f96159d96105afc635c3605dc
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 16:35:38 +0300

    RecyclerView Activity de gerekli kodlamalar yapılarak ilgili sayfada RecyclerView ın gösterilmesi sağlandı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/RecyclerViewActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/RecyclerViewActivity.java
@@ -1,6 +1,7 @@
 package com.ogocer.materialdesignkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,12 +10,41 @@ import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
     private RecyclerView rv;
-    
+    private MyRvAdapter adapter;
+    private ArrayList<String> bilgiList = new ArrayList<>();
+    private ArrayList<Integer> bilgiNoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
         rv = findViewById(R.id.recyclerView);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        bilgiList.add("Bilgi 1");
+        bilgiList.add("Bilgi 2");
+        bilgiList.add("Bilgi 3");
+        bilgiList.add("Bilgi 4");
+        bilgiList.add("Bilgi 5");
+        bilgiList.add("Bilgi 6");
+        bilgiList.add("Bilgi 7");
+        bilgiList.add("Bilgi 8");
+        bilgiList.add("Bilgi 9");
+        bilgiList.add("Bilgi 10");
+
+        bilgiNoList.add(1);
+        bilgiNoList.add(2);
+        bilgiNoList.add(3);
+        bilgiNoList.add(4);
+        bilgiNoList.add(5);
+        bilgiNoList.add(6);
+        bilgiNoList.add(7);
+        bilgiNoList.add(8);
+        bilgiNoList.add(9);
+        bilgiNoList.add(10);
+
+        adapter = new MyRvAdapter(this,bilgiList,bilgiNoList);
+        rv.setAdapter(adapter);
+
     }
 }
