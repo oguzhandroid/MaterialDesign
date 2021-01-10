cdf0459271750b6007da9760d3005844e942a33f
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 17:02:23 +0300

    tasarım kısmına eklediğimiz bottom navigation view Activity kısmında bağlandı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
@@ -1,14 +1,21 @@
 package com.ogocer.materialdesignkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class BottomNavigationActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
     }
 }
