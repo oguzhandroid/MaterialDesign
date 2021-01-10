fd99738b9b19f83fd76ee08db11e7462c086ad8c
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 17:04:39 +0300

    Fragment sınıfından geciciFragment nesnesi oluşturduk vee ilk açıldığında hangi fragmentin gösterileceğini tanımladık.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/BottomNavigationActivity.java
@@ -10,12 +10,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class BottomNavigationActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private Fragment geciciFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
         bottomNavigationView = findViewById(R.id.bottomNav);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_tutucu,new FragmentBirinci()).commit();
+
 
     }
 }
