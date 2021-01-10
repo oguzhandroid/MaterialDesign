a2a3279c102b94a4988334bd970061d26f337029
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 18:32:50 +0300

    TabLayoutActivity de inner class Adapter imizi yazdık.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/TabLayoutActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/TabLayoutActivity.java
@@ -1,7 +1,10 @@
 package com.ogocer.materialdesignkullanimi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
@@ -24,5 +27,23 @@ public class TabLayoutActivity extends AppCompatActivity {
         fragmentListesi.add(new TabFragmentBir());
         fragmentListesi.add(new TabFragmentIki());
         fragmentListesi.add(new TabFragmentUc());
+    }
+
+    public class MyViewPagerAdapter extends FragmentStateAdapter{
+
+        public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragmentListesi.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragmentListesi.size();
+        }
     }
 }
