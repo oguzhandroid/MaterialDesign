df7e6a126f50c0b36db78125a81c4794f3b0654a
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 18:29:56 +0300

    Tab Layout tasarımındaki görsel nesneler ilgili classta bağlandı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/TabLayoutActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/TabLayoutActivity.java
@@ -1,14 +1,24 @@
 package com.ogocer.materialdesignkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 public class TabLayoutActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
     }
 }
