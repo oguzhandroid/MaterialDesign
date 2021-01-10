8802b3af9c298e6f17fdbc0e8e93b7922ecabe35
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 15:56:43 +0300

    menu MainActivity de bağlandı ve themes dosyasına textColor eklendi.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -19,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setSubtitle("Material Design Kullanımı");
 
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+
+        return true;
     }
 }
