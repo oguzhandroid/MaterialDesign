ceea37dfdde1e17e8e8e45a70298665b501ad13f
oguzhandroid <oguzhaneem@gmail.com>
2021-01-10 15:47:45 +0300

    activity_main kısmına toolbar eklendi ve MainActivity de toolbar kodlamaları tamamlandı.

--- app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
+++ app/src/main/java/com/ogocer/materialdesignkullanimi/MainActivity.java
@@ -1,14 +1,23 @@
 package com.ogocer.materialdesignkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Özel Toolbar");
+        toolbar.setTitleTextColor(Color.RED);
+        toolbar.setSubtitle("Material Design Kullanımı");
+
+        setSupportActionBar(toolbar);
     }
 }
